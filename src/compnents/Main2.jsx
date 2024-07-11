b61499fa15a1394f3125262cfd7e7743d1a38e59
ammalgh <ammalgh22@gmail.com>
2024-07-11 14:34:42 +0300

    first commit

--- src/compnents/Main2.jsx
+++ src/compnents/Main2.jsx
@@ -1,24 +1,26 @@
-
-
 function Main2() {
-    return (
-      <div className="flex items-center justify-center p-8 bg-white">
-        <div className="flex flex-col md:flex-row items-center max-w-4xl">
-          <img
-            src="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/freshgoods/ef9d9b9f-freshgoods-about_100000000000000000001o.jpg"
-            alt=""
-            className="w-full md:w-1/2 rounded-md shadow-md"
-          />
-          <div className="mt-8 md:mt-0 md:ml-8 text-left">
-            <h4 className="text-3xl font-semibold mb-4 text-[#9e9b95]">Some more information about your business</h4>
-            <p className="text-lg">
-              Share a little about yourself as a business owner, or maybe describe what makes your product or service unique. Give visitors one more reason to care about your offer and want to buy from you.
-            </p>
-          </div>
+  return (
+    <div className="flex items-center justify-center p-8 bg-white">
+      <div className="flex flex-col md:flex-row items-center max-w-4xl">
+        <img
+          src="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/freshgoods/ef9d9b9f-freshgoods-about_100000000000000000001o.jpg"
+          alt=""
+          className="w-full md:w-1/2 rounded-md shadow-md"
+        />
+        <div className="mt-8 md:mt-0 md:ml-8 text-left">
+          <h4 className="text-3xl font-semibold mb-4 text-[#9e9b95]">
+            Some more information about your business
+          </h4>
+          <p className="text-lg">
+            Share a little about yourself as a business owner, or maybe describe
+            what makes your product or service unique. Give visitors one more
+            reason to care about your offer and want to buy from you.
+          </p>
         </div>
       </div>
-    );
-  }
-  
-  export default Main2;
-  
+      ,/
+    </div>
+  );
+}
+
+export default Main2;
